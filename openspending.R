f205350b7c910ae7ca08b7c4fb2814992e40e2f8
Stefan Kasberger <stefan.kasberger@alpine-geckos.at>
2013-02-23 19:51:45 +0100

    add raw data 2010 and 2009, add descripiton tables for functional and economic numbers, add openspending.org, update openspending.R

--- openspending.R
+++ openspending.R
@@ -1,40 +1,86 @@
-
 ##  INIT  ##
+
+
+rm(list=ls())
 library('stringr')
 
-# data <- read.csv2("./data/raw/test.csv", header=FALSE, stringsAsFactors=FALSE, quote="",
-#                   colClasses=c("character", "character", "character", "character", "integer", "integer", "character", "character", "integer", "integer", "integer", "integer", "integer", "integer", "integer", "integer", "integer", "integer"))
+
+##  FUNCTIONS  ##
 
 
-##  OPEN  ##
-data <- read.csv2("./data/raw/RechnungsabschlussPP1.csv", header=FALSE, quote="\'", stringsAsFactors=FALSE)
-names(data) <- c("Finanzposition", "Bezeichnung", "EinAus", "Teilabschnitt", "RQ", "FG", "SN", "AOB", "AnfZahlungsrueckstand", "LaufendesSoll", "GesamtSoll", "Ist", "SchliesslicheRueckstaende", "Voranschlag", "NachtragsVoranschlage", "GesamtVoranschlag", "UnterschiedLFDSollGesamtVAGuenstiger", "UnterschiedLFDSollGesamtVAUnguenstiger")
-
-##  PREPARE DATA  ##
-tmp <- gsub("\\.", "", data[, 1])       
-tmp <- (sapply(tmp, str_split, ""))
-for(i in seq_along(1:length(tmp))) {
-  for (j in seq_along(1:12)) {
-    test[i, j] <- tmp[[i]][1+j]
+SplitNumber <- function(data, column, year) {
+  data <- sapply(data, str_replace, " ", "")
+  tmp <- gsub("\\.", "", data[, column])       
+  tmp <- (sapply(tmp, str_split, ""))
+  
+  tmpDF <- data.frame(t(sapply(tmp,c)))
+  tmpDF <- tmpDF[, 2:13]
+  
+  names(tmpDF) <- c("haushaltshinweis", "fun1", "fun2", "fun3", "fun4", "fun5", "eco1", "eco2", "eco3", "eco4", "eco5", "eco6")
+  
+  for(i in seq_along(dim(tmpDF)[1])) {
+    funAgg3 <- paste0(tmpDF[, 2], tmpDF[, 3], tmpDF[, 4])
+    ecoAgg3 <- paste0(tmpDF[, 7], tmpDF[, 8], tmpDF[, 9])
+    funAgg5 <- paste0(tmpDF[, 2], tmpDF[, 3], tmpDF[, 4], tmpDF[, 5], tmpDF[, 6])
+    ecoAgg6 <- paste0(tmpDF[, 7], tmpDF[, 8], tmpDF[, 9], tmpDF[, 10], tmpDF[, 11], tmpDF[, 12])
   }
+  
+  year <- as.numeric(rep(as.numeric(year), dim(data)[1]))
+  serial <- 1:dim(data)[1]
+  data <- cbind(serial, data, tmpDF, funAgg3, ecoAgg3, funAgg5, ecoAgg6, as.numeric(year))
+  data
 }
 
-test2 <- test
-names(test2) <- c("zahl", "pol1", "pol2", "pol3", "pol4", "pol5", "eco1", "eco2", "eco3", "eco4", "eco5", "eco6")
 
-for(i in seq_along(dim(test2)[1])) {
-  polAgg3 <- paste0(test2[, 2], test2[, 3], test2[, 4])
-  ecoAgg3 <- paste0(test2[, 7], test2[, 8], test2[, 9])
-  polAgg5 <- paste0(test2[, 2], test2[, 3], test2[, 4], test2[, 5], test2[, 6])
-  ecoAgg6 <- paste0(test2[, 7], test2[, 8], test2[, 9], test2[, 10], test2[, 11], test2[, 12])
-}
+##  2011  ##
 
-serial <- 1:dim(data)[1]
-year <- rep("2011", dim(data)[1])
-data <- cbind(serial, data, test2, polAgg3, ecoAgg3, polAgg5, ecoAgg6, year)
-names(data)[1] <- "serial"
 
-write.csv2(data, file="./data/RechnungsabschlussPP2.csv")
-save(list=ls(), file="./data/rstat/RechnungsabschlussPP2.rda")
+data2011 <- read.csv2("./data/raw/Rechnungsabschluss2011Clean.csv", header=FALSE, quote="\'", 
+                      stringsAsFactors=FALSE, strip.white=TRUE, fileEncoding="iso-8859-1")
+names(data2011) <- c("Finanzposition", "Bezeichnung", "EinAus", "Teilabschnitt", "RQ", "FG", "SN", 
+                     "AOB", "AnfZahlungsrueckstand", "LaufendesSoll", "GesamtSoll", "Ist", "SchliesslicheRueckstaende", 
+                     "Voranschlag", "NachtragsVoranschlage", "GesamtVoranschlag", "UnterschiedLFDSollGesamtVAGuenstiger", 
+                     "UnterschiedLFDSollGesamtVAUnguenstiger")
+
+test <- sapply(data2011$AnfZahlungsrueckstand, str_replace, "\ ", "")
+tmp1 <- SplitNumber(data2011, "Finanzposition", 2011)
+
+
+##  2010  ##
+
+
+data2010 <- read.csv2("./data/raw/Rechnungsabschluss2010Clean.csv", header=FALSE, quote="\'", 
+                      stringsAsFactors=FALSE, strip.white=TRUE, fileEncoding="iso-8859-1")
+names(data2010) <- c("Finanzposition", "Bezeichnung", "EinAus", "Teilabschnitt", "RQ", "FG", "SN", 
+                     "AOB", "AnfZahlungsrueckstand", "LaufendesSoll", "GesamtSoll", "Ist", "SchliesslicheRueckstaende", 
+                     "Voranschlag", "NachtragsVoranschlage", "GesamtVoranschlag", "UnterschiedLFDSollGesamtVAGuenstiger", 
+                     "UnterschiedLFDSollGesamtVAUnguenstiger")
+
+tmp2 <- SplitNumber(data2010, "Finanzposition", 2010)
+
+
+##  2009  ##
+
+
+data2009 <- read.csv2("./data/raw/Rechnungsabschluss2009Clean.csv", header=FALSE, quote="\'", 
+                      stringsAsFactors=FALSE, strip.white=TRUE, fileEncoding="iso-8859-1")
+names(data2009) <- c("Finanzposition", "Bezeichnung", "EinAus", "Teilabschnitt", "RQ", "FG", "SN", "AOB", 
+                     "AnfZahlungsrueckstand", "LaufendesSoll", "GesamtSoll", "Ist", "SchliesslicheRueckstaende", 
+                     "Voranschlag", "NachtragsVoranschlage", "GesamtVoranschlag", "UnterschiedLFDSollGesamtVAGuenstiger", 
+                     "UnterschiedLFDSollGesamtVAUnguenstiger")
+
+tmp3 <- SplitNumber(data2009, "Finanzposition", 2009)
+
+data <- as.data.frame(rbind(tmp1, tmp2, tmp3))
+
+##  WRITE  ##
+
+
+write.csv2(data, file="./data/csv/Rechnungsabschluss2009-2011PP2.csv", row.names=FALSE, fileEncoding="UTF-8")
+save(list=ls(), file="./data/rstat/Rechnungsabschluss2009-2011PP2.rda")
+
+
+
+
 
 
